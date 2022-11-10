5ac1198d914809f5bb523b4e8082e4048525434a
JHKaller <john.kaller@hotmail.de>
2022-11-10 20:15:24 +0100

    back to basics

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,7 @@
-import "../styles/globals.css";
-import { Web3ReactProvider } from "@web3-react/core";
-import { Web3Provider } from "@ethersproject/providers";
-
-const getLibrary = (provider) => {
-  return new Web3Provider(provider);
-};
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <Component {...pageProps} />
-    </Web3ReactProvider>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
