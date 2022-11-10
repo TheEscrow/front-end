d6bcada8e466332b7b8810a434ce5f3c347bd55a
JHKaller <john.kaller@hotmail.de>
2022-11-10 20:22:04 +0100

    Added jobs

--- pages/index.js
+++ pages/index.js
@@ -13,31 +13,31 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://devpost.com/software/theescrow?ref_content=user-portfolio&ref_feature=in_progress">TheEscrow!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by checking out our Jobs!
+          {/* <code className={styles.code}>pages/index.js</code> */}
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h2>Create a logo for me &rarr;</h2>
+            <p>Design a good looking logo for my company.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h2>Build a website &rarr;</h2>
+            <p>Program an interactive website using React!</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2>Take a video for me &rarr;</h2>
+            <p>Film an animal in the wild for my YouTube Channel.</p>
           </a>
 
           <a
@@ -46,9 +46,9 @@ export default function Home() {
             rel="noopener noreferrer"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Spend the day with me &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Become my best friend for a day.
             </p>
           </a>
         </div>
